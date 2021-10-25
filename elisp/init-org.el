4990d84eadb2dff02bfdb728c57d62be1e8228a7
shenxingyu <idream.shen@gmail.com>
2021-10-25 10:34:35 +0800

    org todo and so on

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -1,3 +1,9 @@
 (require 'org)
 
+(setq org-scheduled-past-days 100)
+(setq org-log-done 'time)
+(setq org-directory "~/emacs-files/")
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+
 (provide 'init-org)
