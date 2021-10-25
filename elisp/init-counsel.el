7389bf01b78375bd4bbe05d73b699df5001e7c17
shenxingyu <idream.shen@gmail.com>
2021-10-25 14:51:55 +0800

    change key

--- elisp/init-counsel.el
+++ elisp/init-counsel.el
@@ -2,5 +2,8 @@
 (require 'counsel)
 
 (global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-c f") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
 
 (provide 'init-counsel)
--- elisp/init-project.el
+++ elisp/init-project.el
@@ -1,5 +1,7 @@
 (require 'project)
 
-(define-key project-prefix-map (kbd "g") #'counsel-git-grep)
+(global-set-key (kbd "C-c p") #'project-switch-project)
+(global-set-key (kbd "C-c e") #'project-eshell)
+(global-set-key (kbd "C-c m") #'project-compile)
 
 (provide 'init-project)
