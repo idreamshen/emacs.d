38796707849b136a5a79b4917b4ad4712ddb07f4
shenxingyu <idream.shen@gmail.com>
2021-10-25 15:25:11 +0800

    counsel-dash-at-point

--- elisp/init-counsel-dash.el
+++ elisp/init-counsel-dash.el
@@ -1,5 +1,6 @@
 (package-ensure 'counsel-dash)
 (require 'counsel-dash)
 (setq counsel-dash-common-docsets '("Go"))
+(global-set-key (kbd "C-c d") #'counsel-dash-at-point)
 
 (provide 'init-counsel-dash)
