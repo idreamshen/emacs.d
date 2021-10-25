24f94cc9d08ad604c609017f27648eedc9592692
shenxingyu <idream.shen@gmail.com>
2021-10-25 14:51:39 +0800

    clean tab

--- elisp/init-eshell.el
+++ elisp/init-eshell.el
@@ -2,7 +2,7 @@
 
 (setq eshell-history-size 5000)
 (add-hook 'eshell-first-time-mode-hook
-	  (lambda()
-	    (define-key eshell-mode-map (kbd "C-r") #'counsel-esh-history)))
+          (lambda()
+            (define-key eshell-mode-map (kbd "C-r") #'counsel-esh-history)))
 
 (provide 'init-eshell)
