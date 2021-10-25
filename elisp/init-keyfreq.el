0683d9aff31ef8f818c61dd0804b991329cb9393
shenxingyu <idream.shen@gmail.com>
2021-10-25 10:33:48 +0800

    set excluded commands in keyfreq

--- elisp/init-keyfreq.el
+++ elisp/init-keyfreq.el
@@ -3,4 +3,15 @@
 (keyfreq-mode t)
 (keyfreq-autosave-mode t)
 
+(setq keyfreq-excluded-commands
+      '("self-insert-command"
+        "hungry-delete-backward"
+        "previous-line"
+        "next-line"
+        "newline"
+        "other-window"
+        "magit-section-forward"
+        "ivy-next-line"
+        "backward-char"))
+
 (provide 'init-keyfreq)
