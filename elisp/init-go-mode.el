9246515315b0d8f6208366aed082091c33cc9882
root <root@emacs28.local>
2023-03-29 08:09:24 +0000

    gofmt only in go-mode

--- elisp/init-go-mode.el
+++ elisp/init-go-mode.el
@@ -1,6 +1,10 @@
 (package-ensure 'go-mode)
 (require 'go-mode)
 (setq gofmt-command "goimports")
-(add-hook 'before-save-hook #'gofmt-before-save)
+;(add-hook 'before-save-hook #'gofmt-before-save)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'gofmt-before-save nil 'make-it-local)))
 
 (provide 'init-go-mode)
