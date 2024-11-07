59da27b89197d599043c08afb154ffd8da721b54
idreamshen <idream.shen@gmail.com>
2024-11-07 18:27:02 +0800

    eshell truncate buffer & save history

--- elisp/init-eshell.el
+++ elisp/init-eshell.el
@@ -4,5 +4,9 @@
 (add-hook 'eshell-first-time-mode-hook
           (lambda()
             (define-key eshell-mode-map (kbd "C-r") #'counsel-esh-history)))
+(add-hook 'eshell-output-filter-functions 'eshell-truncate-buffer)
+(setq eshell-hist-ignoredups t)
+
+(run-at-time t 60 'eshell-save-some-history)
 
 (provide 'init-eshell)
