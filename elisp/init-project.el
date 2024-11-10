757200cecc5fae50b1957f8383b33b61b9f0d78c
idreamshen <idream.shen@gmail.com>
2024-11-10 15:24:51 +0800

    add project-shell

--- elisp/init-project.el
+++ elisp/init-project.el
@@ -2,6 +2,7 @@
 
 (global-set-key (kbd "C-c p") #'project-switch-project)
 (global-set-key (kbd "C-c e") #'project-eshell)
+(global-set-key (kbd "C-c s") #'project-shell)
 (global-set-key (kbd "C-c m") #'project-compile)
 
 (provide 'init-project)
