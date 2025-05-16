2e62cca946cc1803849630e690732e8b71ef7015
idreamshen <idream.shen@gmail.com>
2025-05-16 17:43:29 +0800

    update

--- elisp/init-org-drill.el
+++ elisp/init-org-drill.el
@@ -1,4 +1,5 @@
-(package-ensure 'org-drill)
+;(package-ensure 'org-drill)
+(package-ensure-git 'org-drill "https://github.com/idreamshen/org-drill")
 (require 'org-drill)
 
 (provide 'init-org-drill)
