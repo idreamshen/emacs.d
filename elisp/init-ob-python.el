2604d9c41f1b9fddecde94bbf8da104c312355ac
idreamshen <idream.shen@gmail.com>
2025-06-03 11:01:10 +0800

    org babel python command: python3

--- elisp/init-ob-python.el
+++ elisp/init-ob-python.el
@@ -1,7 +1,9 @@
-(package-ensure 'ob-restclient)
+(package-ensure 'ob-python)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  (add-to-list 'org-babel-load-languages '(python . t)))
+
+(setq org-babel-python-command "python3")
 
 (provide 'init-ob-python)
